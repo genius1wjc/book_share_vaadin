a1f74f9b486bdd21e35cd51c60fe5373b31f1864
Jiechao Wang <jiechao@starmobileinc.com>
2018-01-13 22:16:20 -0500

    no message

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -22,36 +22,27 @@ public final class StudentService {
 	 * @param age
 	 */
 	public static void create(int id, String name, int age) {
-		// Create an EntityManager
 		EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
 		EntityTransaction transaction = null;
 
 		try {
-			// Get a transaction
 			transaction = manager.getTransaction();
-			// Begin the transaction
 			transaction.begin();
 
-			// Create a new Student object
 			Student stu = new Student();
 			stu.id = id;
 			stu.name = name;
 			stu.age = age;
 
-			// Save the student object
 			manager.persist(stu);
 
-			// Commit the transaction
 			transaction.commit();
 		} catch (Exception ex) {
-			// If there are any exceptions, roll back the changes
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			// Print the Exception
 			ex.printStackTrace();
 		} finally {
-			// Close the EntityManager
 			manager.close();
 		}
 	}
@@ -65,30 +56,22 @@ public final class StudentService {
 
 		List<Student> students = null;
 
-		// Create an EntityManager
 		EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
 		EntityTransaction transaction = null;
 
 		try {
-			// Get a transaction
 			transaction = manager.getTransaction();
-			// Begin the transaction
 			transaction.begin();
 
-			// Get a List of Students
 			students = manager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
 
-			// Commit the transaction
 			transaction.commit();
 		} catch (Exception ex) {
-			// If there are any exceptions, roll back the changes
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			// Print the Exception
 			ex.printStackTrace();
 		} finally {
-			// Close the EntityManager
 			manager.close();
 		}
 		return students;
@@ -100,33 +83,24 @@ public final class StudentService {
 	 * @param id
 	 */
 	public static void delete(int id) {
-		// Create an EntityManager
 		EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
 		EntityTransaction transaction = null;
 
 		try {
-			// Get a transaction
 			transaction = manager.getTransaction();
-			// Begin the transaction
 			transaction.begin();
 
-			// Get the Student object
 			Student stu = manager.find(Student.class, id);
 
-			// Delete the student
 			manager.remove(stu);
 
-			// Commit the transaction
 			transaction.commit();
 		} catch (Exception ex) {
-			// If there are any exceptions, roll back the changes
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			// Print the Exception
 			ex.printStackTrace();
 		} finally {
-			// Close the EntityManager
 			manager.close();
 		}
 	}
@@ -139,37 +113,27 @@ public final class StudentService {
 	 * @param age
 	 */
 	public static void update(int id, String name, int age) {
-		// Create an EntityManager
 		EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
 		EntityTransaction transaction = null;
 
 		try {
-			// Get a transaction
 			transaction = manager.getTransaction();
-			// Begin the transaction
 			transaction.begin();
 
-			// Get the Student object
 			Student stu = manager.find(Student.class, id);
 
-			// Change the values
 			stu.name = name;
 			stu.age = age;
 
-			// Update the student
 			manager.persist(stu);
 
-			// Commit the transaction
 			transaction.commit();
 		} catch (Exception ex) {
-			// If there are any exceptions, roll back the changes
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			// Print the Exception
 			ex.printStackTrace();
 		} finally {
-			// Close the EntityManager
 			manager.close();
 		}
 	}
